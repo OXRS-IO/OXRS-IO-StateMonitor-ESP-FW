e444e41eb9ae932d76e4656421b8d3dce8ba3daf
Ben Jones <ben.jones12@gmail.com>
2021-11-26 14:38:35 +1300

    update version to 3.3.1 (new release with updated API library)

--- OXRS-SHA-StateMonitor-ESP32-FW.ino
+++ OXRS-SHA-StateMonitor-ESP32-FW.ino
@@ -27,7 +27,7 @@
 #define FW_NAME       "OXRS-SHA-StateMonitor-ESP32-FW"
 #define FW_SHORT_NAME "State Monitor"
 #define FW_MAKER      "SuperHouse Automation"
-#define FW_VERSION    "3.3.0"
+#define FW_VERSION    "3.3.1"
 
 /*--------------------------- Libraries ----------------------------------*/
 #include <Adafruit_MCP23X17.h>        // For MCP23017 I/O buffers
